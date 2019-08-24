dddb385b97e1108ac3f1c8fa811385e8737667de
Thet Paing Tun <thetpaingtun93@gmail.com>
2019-08-24 10:31:31 +0800

    added the puppeteer;

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require('@oclif/command')
+const puppeteer = require('puppeteer')
 
 class WsolCommand extends Command {
   async run() {
-    const {flags} = this.parse(WsolCommand)
+    const { flags } = this.parse(WsolCommand)
     const name = flags.name || 'world'
     this.log(`hello ${name} from ./src/index.js`)
+
+    this.browse()
+  }
+
+
+  async browse() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
   }
 }
 
@@ -15,10 +28,10 @@ Extra documentation goes here
 
 WsolCommand.flags = {
   // add --version flag to show CLI version
-  version: flags.version({char: 'v'}),
+  version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
-  help: flags.help({char: 'h'}),
-  name: flags.string({char: 'n', description: 'name to print'}),
+  help: flags.help({ char: 'h' }),
+  name: flags.string({ char: 'n', description: 'name to print' }),
 }
 
 module.exports = WsolCommand
